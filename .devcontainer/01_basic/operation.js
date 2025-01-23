1dc4a4b7187a6eb993f78cad208adc572a2cd4bd
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-23 15:32:49 +0000

    operation.js

---
+++ .devcontainer/01_basic/operation.js
@@ -0,0 +1,23 @@
+// operations in javascript
+let value=3;
+let negvalue=-value;
+// console.log(negvalue);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); //2 raise to power 2 ans is 4
+console.log(2**3); //2 raise to power 2 ans is 8
+console.log(2/2);
+let str1="hello";
+let str2=" Nrupesh";
+let str3=str1+str2;
+console.log(str3);
+console.log(true);//ans true
+console.log(+true);//ans=1
+// console.log(true+);//error
+
+// let num1,num2,num3;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
