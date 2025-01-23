ae2203f945a55203b5992198329937f1ce0a15e2
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-23 11:20:37 +0000

    03_datatypechange.js

---
+++ .devcontainer/01_basic/03_datatypechange.js
@@ -0,0 +1,12 @@
+let score="33";
+console.log(typeof(score));
+let valueinNumber=Number(score);
+console.log(typeof(valueinNumber));
+//  "33"=>33
+// "33qbc"=>NAN
+//  true=>1;false=0;
+let isloggedIn=1;
+let booleanIsLoggedIn=Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);
+//""=>false
+// "Nrupesh"=>true
