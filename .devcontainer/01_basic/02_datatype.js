ceb1f15f3d587681664fb3102f5dfe7440862c50
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-23 10:52:36 +0000

    02_datatype.js

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,18 @@
+let name="Nrupesh";
+let age=18;
+let isLoggedIn=false;
+
+//datatypes
+/*
+1 number
+2 string
+3 boolean == ture/false
+4 null=> standalone value
+5 undefined6
+6 symbol // uniqueness
+
+*/
+
+// object
+console.log(typeof(age));
+console.log(typeof(null));
