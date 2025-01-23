8447a7bfc3860a1f2571e5022d1636ceec7a4d19
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-23 10:47:59 +0000

    01_variable.js

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId=144553;
+let accountEmail="sham@gmail.com";
+var accountPassword="12345";
+accountCity="jaipur";
+// accountId=2;not allowed
+accountEmail="hc@gmail.com";
+accountPassword="21212121";
+accountCity="mumbai";
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,]);
+// prefer not to use var because of issue in blog scope and functional scope 
