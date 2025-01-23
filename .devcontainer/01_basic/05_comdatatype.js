c09240b2d225a9100ca8c60e05b3b5105f527b58
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-23 16:09:02 +0000

    05_comdatatype.js

---
+++ .devcontainer/01_basic/05_comdatatype.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);//true
+// console.log("02">1);//true
+// console.log(undefined==0);//false
+// console.log(undefined>0);//false
+// console.log(undefined<0);//false
+
+// === strict check it check the value and its data type 
+
+console.log("2"===2);
