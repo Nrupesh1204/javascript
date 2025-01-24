c1f8bf5fed7ebe3227eb3b9924a8129f627e6512
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-24 16:43:50 +0000

    07_string.js

---
+++ .devcontainer/01_basic/07_string.js
@@ -0,0 +1,18 @@
+const name="Nrupesh";
+const repoCount=50;
+// console.log(name+repoCount+"value");
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+// strings
+const gameName=new String('Nrupe-sh');
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('p'));
+const newString=gameName.substring(0,4);
+console.log(newString);
+const url="https://Nrupesh.com/Nrupesh%20Kamble";
+console.log(url.replace('%20','-'));
+
