de8624f64b67a6459c6c9cd242c82fcc7a8fbad8
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-25 09:14:56 +0000

    09_array.js

---
+++ .devcontainer/01_basic/09_array.js
@@ -0,0 +1,32 @@
+// arrays
+const myArr=[0,1,2,3,4,5];
+// console.log(myArr[0]);
+ const myHeroes=["Nrupesh","ironman","Thor"];
+ //console.log(myHeroes);
+
+//  javascripts array copy create shallow copies
+const myArr2=new Array(0,1,45,6,7);
+//console.log(myArr2);
+//Array Methods
+// myArr.push(6);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+// myArr.unshift(9);//9 is shifted at the first index
+// console.log(myArr);
+// console.log(myArr.includes(9));//boolean
+// console.log(myArr);
+
+
+// slice ,splice method in javascript
+console.log("A",myArr);
+const myn1=myArr.slice(1,3);
+console.log(myn1);
+console.log("B",myArr);
+
+// splice
+// console.log("A",myArr);
+// const myn2=myArr.splice(1,3);
+// console.log(myn2);
+// console.log("C",myArr);
+// console.log("B",myArr);
