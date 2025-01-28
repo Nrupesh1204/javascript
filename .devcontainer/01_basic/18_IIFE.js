e8acfa3db22551c80c05b2b93d187d02d4c082ed
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-28 16:40:03 +0000

    18_IIFE.js

---
+++ .devcontainer/01_basic/18_IIFE.js
@@ -0,0 +1,11 @@
+// Immediately Invoked function Expression (IIFE)
+// global scope pollution problem the variable are removed for that purpose
+
+(function chai()
+{//named IIFE
+    console.log(`DB CONNECTED`);
+})()
+
+// ()()
+// first parenthesis for defenation 
+// second parenthesis for excecution call
