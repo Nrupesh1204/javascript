7f8725624c97f3c8aed071e7ad3f2e3ac51fd375
Nrupesh1204 <kamblenrupesh@gmail.com>
2025-01-29 03:39:37 +0000

    19_controlflow.js

---
+++ .devcontainer/01_basic/19_controlflow.js
@@ -0,0 +1,28 @@
+
+
+                                //    Execution Phase
+// 1 Global Execution 
+// 2  Memory Phase
+// val1->undefined
+// val2->undefined
+// addNum->defination 
+// result1->undefined
+// result2->undefined
+// 3 Execution Phase
+
+// val1<-10,
+// val<-5
+// 3 Exection phase
+
+
+// code1-
+
+// let val1=10;
+// let val2=5;
+// function addNum(num1,num2)
+// {
+//     let total=num1+num2;
+//     return total
+// }
+// let result1=addNum(val1,val2);
+// let result2=addNum(10,2);
